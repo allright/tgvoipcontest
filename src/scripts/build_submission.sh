e54b804891d284271a44e97722af9627317f99db
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-19 13:53:15 +0300

    fix buildscripts

--- src/scripts/build_submission.sh
+++ src/scripts/build_submission.sh
@@ -6,13 +6,15 @@ mkdir submission
 cp -a src submission/src
 find ./submission -type d -name '.*' -exec rm -rf {} +
 find ./submission -type f -name '.*' -exec rm {} +
+rm -rf ./submission/src/tgvoiprate/cmake-build-debug
+rm -rf ./submission/src/tgvoipcall/cmake-build-debug
 rm -rf ./submission/src/tgvoipcallpy/venv
 zip -r submission.zip submission
 rm -rf ./submission
 
 docker build -t tgvoipcontestbuild -f ./src/scripts/DockerfileBuildSubmission .
 rm -rf ./docker_build
-docker run -v ${PWD}/.docker_build:/out tgvoipcontestbuild cp ./tgvoiprate /out
+docker run -v ${PWD}/.docker_build:/out tgvoipcontestbuild cp ./tgvoiprate/tgvoiprate /out
 cd ./.docker_build
 mkdir ./submission
 mv ./tgvoiprate ./submission/

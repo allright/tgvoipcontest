ab3b84087fafdc99308514053112a185eeb3402b
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-19 12:20:07 +0300

    fixes

--- src/scripts/run_test_submission.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run tgvoipcontest tgvoipcallpy/tgvoipcallpy.py
-
---
+++ src/scripts/test_submission.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker run tgvoipcontest ls
+docker run tgvoipcontest ./src/tgvoipcallpy/tgvoipcallpy.py
+docker run tgvoipcontest ./tgvoiprate

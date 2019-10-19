8e7b24c996e1255c775207fbbf5cdfe1b7dde455
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-19 10:42:44 +0300

    remove unneded . files

--- src/scripts/build_submission.sh
+++ src/scripts/build_submission.sh
@@ -4,6 +4,7 @@ mkdir ../../submission
 cp -a ../../src ../../submission/src
 cd ../../
 find ./submission -type d -name '.*' -exec rm -rf {} +
+find ./submission -type f -name '.*' -exec rm {} +
 rm -rf ./submission/src/tgvoipcallpy/venv
 zip -r submission.zip submission
 rm -rf ./submission

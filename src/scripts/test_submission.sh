9a27dec6be88c2595d8d4d4dbd8bb8b3bd8d41fe
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-22 17:41:01 +0300

    working linux

--- src/scripts/test_submission.sh
+++ src/scripts/test_submission.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker run tgvoipcontest ls
-docker run tgvoipcontest ./src/tgvoipcallpy/tgvoipcallpy.py
-docker run -v ${PWD}/../../testdata:/testdata tgvoipcontest ./tgvoiprate /testdata/in.wav /testdata/out.wav
-docker run -v ${PWD}/../../testdata:/testdata tgvoipcontest ./tgvoipcall /testdata/in.wav /testdata/out_test.wav
+#docker run tgvoipcontest ./src/tgvoipcallpy/tgvoipcallpy.py
+docker run -v ${PWD}/../../testdata:/testdata tgvoipcontest ./tgvoipcall /testdata/samples/sample17_0d1f6a407f028a451f3a6a90098a9300.ogg /testdata/out.ogg
+docker run -v ${PWD}/../../testdata:/testdata tgvoipcontest ./tgvoiprate /testdata/samples/sample17_0d1f6a407f028a451f3a6a90098a9300.ogg /testdata/out.ogg

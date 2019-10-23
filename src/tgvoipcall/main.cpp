545134d14cc141285b062003eb70bd198af5a518
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-23 12:04:44 +0300

    loopback test

--- src/tgvoipcall/main.cpp
+++ src/tgvoipcall/main.cpp
@@ -42,76 +42,72 @@ private:
     IPCMDest& _dest;
 };
 
-int main (int argc, const char *argv []) {
-
-    auto testWavFileIn = argv[1];
-    auto testWavFileOut = argv[2];
-
-    auto orig = IPCMSource::openOggFile(testWavFileIn, 48000);
-    auto dest = IPCMDest::openOggWriter(testWavFileOut);
-
+void loopbackTest( const char *in, const char* out) {
+    auto orig = IPCMSource::openOggFile(in, 48000);
+    auto dest = IPCMDest::openOggWriter(out);
 
     test::MockReflector reflector("127.0.0.1", 1033);
     reflector.Start();
 
-  //  for(int i=0;i<10;i++){
-       // webrtc::WavReader wavReader(testWavFileIn);
-        PCMReader wavReader(*orig);
-        PCMWriter wavWriter(*dest);
-      //  webrtc::WavWriter wavWriter(testWavFileOut, 48000, 1);
+    PCMReader wavReader(*orig);
+    PCMWriter wavWriter(*dest);
 
-        VoIPController controller1;
-        VoIPController controller2;
+    VoIPController controller1;
+    VoIPController controller2;
 
 
-        std::array<std::array<uint8_t, 16>, 2> peerTags=test::MockReflector::GeneratePeerTags();
-        std::vector<Endpoint> endpoints1;
-        IPv4Address localhost("127.0.0.1");
-        IPv6Address emptyV6;
-        endpoints1.push_back(Endpoint(1, 1033, localhost, emptyV6, Endpoint::Type::UDP_RELAY, peerTags[0].data()));
-        controller1.SetRemoteEndpoints(endpoints1, false, 76);
-        std::vector<Endpoint> endpoints2;
-        endpoints2.push_back(Endpoint(1, 1033, localhost, emptyV6, Endpoint::Type::UDP_RELAY, peerTags[1].data()));
-        controller2.SetRemoteEndpoints(endpoints2, false, 76);
+    std::array<std::array<uint8_t, 16>, 2> peerTags=test::MockReflector::GeneratePeerTags();
+    std::vector<Endpoint> endpoints1;
+    IPv4Address localhost("127.0.0.1");
+    IPv6Address emptyV6;
+    endpoints1.push_back(Endpoint(1, 1033, localhost, emptyV6, Endpoint::Type::UDP_RELAY, peerTags[0].data()));
+    controller1.SetRemoteEndpoints(endpoints1, false, 76);
+    std::vector<Endpoint> endpoints2;
+    endpoints2.push_back(Endpoint(1, 1033, localhost, emptyV6, Endpoint::Type::UDP_RELAY, peerTags[1].data()));
+    controller2.SetRemoteEndpoints(endpoints2, false, 76);
 
-        char encryptionKey[256];
-        RAND_bytes((uint8_t*)encryptionKey, sizeof(encryptionKey));
-        controller1.SetEncryptionKey(encryptionKey, true);
-        controller2.SetEncryptionKey(encryptionKey, false);
+    char encryptionKey[256];
+    RAND_bytes((uint8_t*)encryptionKey, sizeof(encryptionKey));
+    controller1.SetEncryptionKey(encryptionKey, true);
+    controller2.SetEncryptionKey(encryptionKey, false);
 
+    controller1.SetAudioDataCallbacks([&wavReader](int16_t* data, size_t len){
+        wavReader.ReadSamples(len, data);
+    }, [](int16_t* data, size_t len){
 
+    });
 
-        controller1.SetAudioDataCallbacks([&wavReader](int16_t* data, size_t len){
-            wavReader.ReadSamples(len, data);
-        }, [](int16_t* data, size_t len){
+    controller2.SetAudioDataCallbacks([](int16_t* data, size_t len){
 
-        });
+    }, [&wavWriter](int16_t* data, size_t len){
+        wavWriter.WriteSamples(data, len);
+    });
 
-        controller2.SetAudioDataCallbacks([](int16_t* data, size_t len){
+    controller1.Start();
+    controller2.Start();
+    usleep(1000000);
+    controller1.Connect();
+    controller2.Connect();
 
-        }, [&wavWriter](int16_t* data, size_t len){
-            wavWriter.WriteSamples(data, len);
-        });
+    usleep(20000000);
 
-;
-        controller1.Start();
-        controller2.Start();
-        controller1.Connect();
-        controller2.Connect();
-
-        usleep(20000000);
-//        usleep(1000000);
-//        reflector.SetDropAllPackets(false);
-//        usleep(6000000);
-//        reflector.SetDropAllPackets(false);
-//        usleep(4000000);
-
-    //    [NSThread sleepForTimeInterval:3.0];
-        controller1.Stop();
-        controller2.Stop();
-
-  //  }
+    controller1.Stop();
+    controller2.Stop();
 
     reflector.Stop();
+}
+
+int main (int argc, const char *argv []) {
+
+    if (argc < 4) {
+        printf("invalid number of args\n");
+        return -1;
+    }
+
+    if (argv[1][0] == '-') {
+        loopbackTest( argv[2],argv[3]);
+    }
+
+
     return 0;
 }
